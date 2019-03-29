412af2c08e26ed93c27ee49d91707d02a75f2d61
kidkang <723616880@qq.com>
2019-03-29 10:41:38 +0800

    add func pageForAnt

--- src/Tool.php
+++ src/Tool.php
@@ -11,5 +11,15 @@ class Tool{
     public static function rf($message){
         echo json_encode(['code'=>'ERROR','message'=>$message]);
     }
+    //laravel 返回 
+    public static function pageForAnt($data){
+        //current pageSize total
+        $page =[];
+        $page['current'] = (int) $data['current_page'];
+        $page['total'] = (int) $data['total'];
+        $page['pageSize'] = (int) $data['per_page'];
+        return $page;
+    }
 }
+
 ?>
---
+++ test/testPageForAnt.php
@@ -0,0 +1,21 @@
+<?php 
+require_once '../vendor/autoload.php';
+use Cate\Tool;
+$page = [
+    'current_page' => 1,
+    'first_page_url' => 'http://127.0.0.1:8000/template?page=1',
+    'from' => 1,
+    'last_page' => 1,
+    'last_page_url' => 'http://127.0.0.1:8000/template?page=1',
+    'next_page_url' => 1,
+    'path' => 'http://127.0.0.1:8000/template',
+    'per_page' => 1,
+    'prev_page_url' => 1,
+    'to' => 1,
+    'total' => 10
+];
+
+
+$page = Tool::pageForAnt($page);
+print_r($page);
+?>
